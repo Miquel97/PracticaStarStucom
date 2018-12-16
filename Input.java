b59f22fbaafcca50aae8a579d016227c5df178c8
Miquel97 <45920481+Miquel97@users.noreply.github.com>
2018-12-16 18:13:58 +0100

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Input;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Miquel
+ */
+public class Input {
+    
+    /**
+     * Metodo para pedir String.
+     *
+     * @param texto
+     * @return
+     */
+    public static String pedirCadena(String texto) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String cadena = "";
+        do {
+            try {
+                System.out.println(texto);
+                cadena = br.readLine();
+                if (cadena.equals("")) {
+                    System.out.println("No se puede dejar el campo en blanco.");
+                }
+            } catch (IOException ex) {
+                System.out.println("Error de entrada / salida.");
+            }
+        } while (cadena.equals(""));
+        return cadena;
+    }
+
+    /**
+     * Metodo para pedir entero.
+     *
+     * @param texto
+     * @return
+     */
+    public static int pedirEntero(String texto) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = 0;
+        boolean error;
+        do {
+            try {
+                System.out.println(texto);
+                num = Integer.parseInt(br.readLine());
+                error = false;
+            } catch (IOException ex) {
+                System.out.println("Error de entrada / salida.");
+                error = true;
+            } catch (NumberFormatException ex) {
+                System.out.println("Debes introducir un número entero.");
+                error = true;
+            }
+        } while (error);
+        return num;
+    }
+}
